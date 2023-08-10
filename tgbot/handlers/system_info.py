918d2a637cf851593ae0df62a81fa3777c71efbe
izzat1998 <izzattilla706@gmail.com>
2023-08-10 16:08:20 +0500

    monitor bot add service check

--- tgbot/handlers/system_info.py
+++ tgbot/handlers/system_info.py
@@ -5,6 +5,8 @@ import psutil as psutil
 from aiogram import Dispatcher, types
 from aiogram.types import Message
 
+from tgbot.keyboards.reply import service_keyboard
+
 
 async def get_system_info(message: Message):
     cpu_percent = psutil.cpu_percent()
@@ -42,6 +44,29 @@ async def check_service(message: types.Message):
     await message.answer(TEXT, parse_mode='HTML')
 
 
+async def show_logs(message: Message):
+    service_name = message.text
+    cmd = ['journalctl', '--unit', f'{service_name}.service', '-n', '25']  # Last 25 lines of logs for the service
+    result = await asyncio.to_thread(subprocess.run, cmd, capture_output=True, text=True)
+    if result.stdout:
+        await message.answer(f"Logs for {service_name}:\n\n{result.stdout}")
+    else:
+        await message.answer(f"No logs found for {service_name} or an error occurred.")
+
+
+async def service_logs(message: types.Message):
+    await message.answer("Choose an option:", reply_markup=service_keyboard)
+
+
 def register_system_info(dp: Dispatcher):
     dp.register_message_handler(get_system_info, commands=["system_info"], state="*")
     dp.register_message_handler(check_service, commands=["check_service"], state="*")
+    dp.register_message_handler(service_logs, commands=["service_logs"], state="*")
+    dp.register_message_handler(show_logs,
+                                lambda message: message.text in [
+                                                                 'order',
+                                                                 'telegram bot',
+                                                                 'image',
+                                                                 'celery',
+                                                                 'redis'],
+                                state="*")
---
+++ tgbot/keyboards/reply.py
@@ -0,0 +1,15 @@
+from aiogram.types import ReplyKeyboardMarkup
+from telegram import KeyboardButton
+
+service_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+button1 = KeyboardButton("order")
+button2 = KeyboardButton("telegram bot")
+button3 = KeyboardButton("image")
+button4 = KeyboardButton("celery")
+button5 = KeyboardButton("redis")
+
+# Add buttons to the keyboard
+service_keyboard.add(button1, button2)
+service_keyboard.add(button3)
+service_keyboard.add(button4, button5)
