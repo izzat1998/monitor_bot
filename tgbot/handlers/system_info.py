cccbcde4e54dcddaf09ee928baa0b23b8faae2ec
izzat1998 <izzattilla706@gmail.com>
2023-08-10 15:41:46 +0500

    monitor bot add service check

--- tgbot/handlers/system_info.py
+++ tgbot/handlers/system_info.py
@@ -23,7 +23,21 @@ async def check_service(message: types.Message):
     # Splitting the message to get arguments
     # Since subprocess.run isn't async, we use run_in_executor to not block the event loop
     for service_name in SERVICES:
-        TEXT += f"{service_name}: {await asyncio.to_thread(subprocess.run, ['systemctl', 'status', service_name], capture_output=True, text=True)}\n"
+        result = await asyncio.to_thread(subprocess.run, ['systemctl', 'status', service_name], capture_output=True,
+                                         text=True)
+        if result.returncode == 0:
+            status = "is currently running"
+        elif result.returncode == 1:
+            status = "is currently stopped,error occured"
+        elif result.returncode == 2:
+            status = "is loaded, but stopped."
+        elif result.returncode == 3:
+            status = "is not loaded"
+        elif result.returncode == 4:
+            status = " reports a status of dead"
+        else:
+            status = "is in an unknown state"
+        TEXT += f"{service_name}: is act {status}\n"
 
     await message.answer(TEXT)
 
