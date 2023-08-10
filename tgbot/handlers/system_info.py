f0ea04c8b85e221aa18829e7cef77bda15a09a4b
izzat1998 <izzattilla706@gmail.com>
2023-08-10 12:38:26 +0500

    monitor bot add service check

--- tgbot/handlers/system_info.py
+++ tgbot/handlers/system_info.py
@@ -1,5 +1,8 @@
+import asyncio
+import subprocess
+
 import psutil as psutil
-from aiogram import Dispatcher
+from aiogram import Dispatcher, types
 from aiogram.types import Message
 
 
@@ -14,5 +17,26 @@ async def get_system_info(message: Message):
     await message.reply(text, parse_mode='Markdown')
 
 
+async def check_service(message: types.Message):
+    # Splitting the message to get arguments
+    args = message.text.split()[1:]
+
+    if len(args) == 0:
+        await message.reply("Please provide a service name. Usage: /check_service <service_name>")
+        return
+
+    service_name = args[0]
+
+    # Since subprocess.run isn't async, we use run_in_executor to not block the event loop
+    result = await asyncio.to_thread(subprocess.run, ['systemctl', 'is-active', service_name], capture_output=True,
+                                     text=True)
+
+    if result.returncode == 0:
+        await message.reply(f"{service_name} is active.")
+    else:
+        await message.reply(f"{service_name} is not active or doesn't exist.")
+
+
 def register_system_info(dp: Dispatcher):
     dp.register_message_handler(get_system_info, commands=["system_info"], state="*")
+    dp.register_message_handler(check_service, commands=["check_service"], state="*")
